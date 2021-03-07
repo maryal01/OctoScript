42b4ba08c3b35d2af7ce9397a5beb7c646f14b20
Manish Aryal <manish.aryal@tufts.edu>
2021-03-07 14:10:33 -0500

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -2,21 +2,28 @@ type boolOp = AND | OR
 type binaryOp = Add | Sub | Mul | Div | Pow | Log
 type unaryOp = NOT | NEG
 type compOp  = GT | GTE | LT | LTE | EQ | NEQ
-type dtype = INT | FLOAT | STRING | TABLE | LIST | TUPLE | LAMBDA
+type dtype = INT | FLOAT | STRING | LAMBDA
+type data = TABLE | TUPLE | LIST
+type func =  string list * statement list 
 (* the binary values of true and false are 1 and 0 respectively *)
+
+type prim = 
+    | Int of int
+    | String of string
+    | Float of float
 
 type expr = 
 	|Binop of expr * binaryOp * expr
 	|Boolop of expr * boolOp * expr
 	|Unop of unaryOp * expr 
 	|Comop of expr * copmOp * expr
-	|Lit of int
+	|Lit of prim
+	|DataStruct of data
 	|Var of string
-	|DataStruct of string (* calling a separate type *)
-	|Lambda of expr * expr (* function name, datastruct of string AS arguments, dtype AS returntype *)
-	|Apply of string * string * expr (* data structure name, datastructure's function name,  datastruct of string AS arguments,*)
-	|Func of string * expr (* *dtype?? if require strict type checking *)
-
+	|Lambda of func (*|| parameters * body ||*)
+	|Apply of expr * func  (*|| (DataStruct of data) * func   ||*)
+	|FuncCall of func * expr list (*|| func_name * parameters ||*)
+ 
 type statement =
 	|While  of expr * statement
 	|If of expr * statement * statement
@@ -24,31 +31,5 @@ type statement =
 	|Break
 	|Assign of string * expr
 	|Print  of string
-	|Expr
-	|Function of string * expr * expr * dtype
-
-type datastruct = 
-    |Table of string * ??
-	|Tuple of string * ??
-	|List  of string * ??
-
-
-(*
-Lambda: (a, b) -> {expr}
-arguments
-expression as body
-
-Functions:
-name of function
-arguments
-expression as body
-return type
-
-
-Function Call:
-name of function
-arguments
-
-Expressions:
-Funcall (e.g. function())
-*)
+	|Expr of expr
+	|Function of string * func * dtype
