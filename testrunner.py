b1b895dbd471e9e5f7daa55a7539e360f04253f4
Danielle Lan <19304417+ASingleCabbage@users.noreply.github.com>
2021-03-10 02:55:25 -0500

    Create testrunner.py

---
+++ testrunner.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+
+# usage: ./testrunner <file> (star operator works)
+
+import sys
+import glob
+import subprocess
+
+statPass = 0
+statFail = 0
+
+tests = glob.glob(sys.argv[1])
+for t in tests:
+    result = subprocess.run(['./toplevel.native', '<', 'cat', t], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if(result.returncode != 0):
+        statFail += 1
+        print('test {} failed with exit code {}...\nstdout:{}\nstderr:{}'.format(t, result.returncode, result.stdout, result.stderr))
+    else:
+        statPass += 1
+        print('test {} passed')
+
+print('{} tests run, {} passes, {} fails'.format(len(tests), statPass, statFail))
