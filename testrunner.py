26edcff8e4eb5478f9b5eca490645b86350a837b
Danielle Lan <daniellebluish@gmail.com>
2021-03-11 22:15:58 -0500

    toplevel tests parser as well

--- testrunner.py
+++ testrunner.py
@@ -10,7 +10,7 @@ statFail = 0
 
 tests = sys.argv[1:]
 for t in tests:
-    result = subprocess.run(['./toplevel.native', '<', 'cat', t], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.run(['cat', t, '|', './toplevel.native'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if(result.returncode != 0):
         statFail += 1
         print('test {} failed with exit code {}...\nstdout:{}\nstderr:{}'.format(t, result.returncode, result.stdout, result.stderr))
