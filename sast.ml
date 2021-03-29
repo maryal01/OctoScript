63092775868e22feb82e66bed868b22004b6836e
Manish Aryal <manish.aryal@tufts.edu>
2021-03-29 10:18:34 -0400

    prettfying semant.ml

--- sast.ml
+++ sast.ml
@@ -1,39 +1,39 @@
 open Ast
 
 type sexpr = typ * sx
-and sx = 
-    SIntLit of int
+
+and sx =
+  | SIntLit of int
   | SFloatLit of float
   | SStringLit of string
   | SBoolLit of bool
   | SListLit of typ * prim list
   | STupleLit of typ list * prim list
-  | STableLit of prim list * prim list
+  | STableLit of typ list * prim list list
   | SBinop of sexpr * binaryOp * sexpr
-  | SUnop of  unaryOp * sexpr
+  | SUnop of unaryOp * sexpr
   | SIfExpr of sexpr * sexpr * sexpr
   | SLambda of bind list * sexpr
   | SVar of string
-  | SApply of sexpr * string & sexpr list
+  | SApply of sexpr * string * sexpr list
   | SCall of string * sexpr list
   | SNoExp
 
-type sstmt = 
-  | SWhile  of sexpr * stmnt list
+type sstmt =
+  | SWhile of sexpr * stmnt list
   | SIf of sexpr * sstmt list * sstmt list
   | SReturn of sexpr
   | SBreak
   | SDeclare of typ * string * sexpr
   | SAssign of string * sexpr
-  | SPrint  of sexpr
+  | SPrint of sexpr
   | SExpr of sexpr
 
-
 type sfunc_decl = {
-	styp: typ;
-	sfname: string;
-	sformals: bind list;
-	sbody: sstmt list;
+  styp : typ;
+  sfname : string;
+  sformals : bind list;
+  sbody : sstmt list;
 }
 
 type sprogram = sfunc_decl list * sstmt list
