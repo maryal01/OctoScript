0acf3f9828e648e8ff595b423fd9ad90063dd637
Manish Aryal <manish.aryal@tufts.edu>
2021-03-30 13:32:29 -0400

    updating toplevel.ml

--- toplevel.ml
+++ toplevel.ml
@@ -20,4 +20,4 @@ let () =
   let lexbuf = Lexing.from_channel !channel in
   let ast = Parser.program Scanner.token lexbuf in
   let sast = Semant.check ast in
-  print_string ( Sast.string_of_sprogram sast)
+  ()
