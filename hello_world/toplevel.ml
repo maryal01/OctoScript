1616b036d141825f2bc65e11d846db483d089579
Manish Aryal <manish.aryal@tufts.edu>
2021-04-06 16:28:36 -0400

    final folder ready

---
+++ hello_world/toplevel.ml
@@ -0,0 +1,32 @@
+type action = Ast | Sast | LLVM_IR | Compile
+
+let () =
+  let action = ref Compile in
+  let set_action a () = action := a in
+  let speclist =
+    [
+      ("-a", Arg.Unit (set_action Ast), "Print the AST");
+      ("-s", Arg.Unit (set_action Sast), "Print the SAST");
+      ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
+      ( "-c",
+        Arg.Unit (set_action Compile),
+        "Check and print the generated LLVM IR (default)" );
+    ]
+  in
+  let usage_msg = "usage: ./toplevel.native [-a|-s|-l|-c] [file.mc]" in
+  let channel = ref stdin in
+  Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
+
+  let lexbuf = Lexing.from_channel !channel in
+  let ast = Parser.program Scanner.token lexbuf in
+  match !action with
+      Ast -> raise (Failure "action not implemented")
+    | _   -> let sast = Semant.check ast in
+                match !action with
+                    Ast     -> ()
+                  | Sast    -> raise (Failure "action not implemented")
+                  | LLVM_IR -> raise (Failure "action not implemented")
+                  | Compile -> 
+                      let m = Codegen.translate sast in
+                        Llvm_analysis.assert_valid_module m;
+                        print_string (Llvm.string_of_llmodule m)
