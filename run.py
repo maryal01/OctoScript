7e6dcb627a5d6f9fbeb98d6d04477b250338bb49
Danielle Lan <daniellebluish@gmail.com>
2021-04-06 15:10:36 -0400

    hello test script works

--- run.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-p1 = subprocess.run(["./toplevel.native", "test.oc", "|", "tee" "test.ll"])
-
-p2 = subprocess.run(["llc", "test.ll"])
-
-p3 = subprocess.run(["gcc", "test.s"])
-
-p4 = subprocess.run(["./a.out"])
