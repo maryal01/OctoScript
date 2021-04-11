2755306ea5aaffba1e5043121e47abdcfa77d95a
Danielle Lan <hao-wei.lan@tufts.edu>
2021-04-11 19:15:42 -0400

    added predef.ml for better predef function management

---
+++ predef.ml
@@ -0,0 +1,16 @@
+module A = Ast
+module S = Sast
+
+module Set = Set.Make
+
+(* Fixed or Var indicates normal parameters or varargs *)
+type params = Fixed of A.typ list | Var of A.typ list
+
+(* OctoScript name, C name, return type, parameter list *)
+type predef_func = string * string * A.typ * params
+
+let predefs = 
+  [("print", "printf", A.INT, (Var [A.STRING]))]
+
+let predef_names = 
+  List.map (fun (n,_,_,_)-> n) predefs
