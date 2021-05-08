1815edc0cb0e59403fddd09e683b6a09453538b1
Danielle Lan <hao-wei.lan@tufts.edu>
2021-05-08 16:22:07 -0400

    added file stdlib.oc and functionality in toplevel to write preloaded oc code

--- toplevel.ml
+++ toplevel.ml
@@ -13,20 +13,23 @@ let () =
         "Check and print the generated LLVM IR (default)" );
     ]
   in
-  let usage_msg = "usage: ./toplevel.native [-a|-s|-l|-c] [file.mc]" in
+  let usage_msg = "usage: ./toplevel.native [-a|-s|-l|-c] [file1.oc]" in
   let channel = ref stdin in
   Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
 
+  let lib_decls, lib_stmtss = Parser.program Scanner.token (Lexing.from_channel (open_in "stdlib.oc")) in
+
   let lexbuf = Lexing.from_channel !channel in
-  let ast = Parser.program Scanner.token lexbuf in
-  match !action with
+  let prog_decls, prog_stmts = Parser.program Scanner.token lexbuf in
+  let ast = (lib_decls @ prog_decls, lib_stmtss @ prog_stmts) in
+  (match !action with
       Ast -> raise (Failure "action not implemented")
     | _   -> let sast = Semant.check ast in
-                match !action with
+                (match !action with
                     Ast     -> ()
                   | Sast    -> raise (Failure "action not implemented")
                   | LLVM_IR -> raise (Failure "action not implemented")
                   | Compile -> 
                       let m = Codegen.translate sast in
                         Llvm_analysis.assert_valid_module m;
-                        print_string (Llvm.string_of_llmodule m)
+                        print_string (Llvm.string_of_llmodule m)))
